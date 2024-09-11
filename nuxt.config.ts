288c176d3d28400c67e966c636fa52766d35d445
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-09-11 18:55:18 +0300

    del router

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  router: {
-    base: '/qtim-test/'
- },
 })
